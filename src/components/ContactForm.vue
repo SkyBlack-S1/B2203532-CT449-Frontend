89b51e490061cfba14d005915e2be48c121656ae
SkyBlack-S1 <dinhthuan836@gmail.com>
2025-07-02 01:07:50 +0700

    Tao trang them moi lien he

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -52,14 +52,16 @@
       </label>
     </div>
     <div class="form-group">
-      <button class="btn btn-primary">Lưu</button>
+      <button class="btn btn-primary">
+        <i class="fa-solid fa-floppy-disk"></i> Lưu
+      </button>
       <button
         v-if="contactLocal._id"
         type="button"
         class="ml-2 btn btn-danger"
         @click="deleteContact"
       >
-        Xóa
+        <i class="fa-solid fa-trash"></i> Xóa
       </button>
       <button type="button" class="ml-2 btn btn-danger" @click="Cancel">
         Thoát
